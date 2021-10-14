ec447e1403731c8c5b0b0c8cff54b0f592d0c99f
abdullahbeker <abdullahbeker.54@hotmail.com>
2021-10-14 22:48:06 +0300

    add docker hub as the repository to be logged in

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-docker login -u "$DOCKER_HUB_LOGIN" -p "$DOCKER_HUB_TOKEN"
+docker login -u "$DOCKER_HUB_LOGIN" -p "$DOCKER_HUB_TOKEN" docker.io
 docker build . --file Dockerfile --tag repsy/repsy-landing:"$1"
 docker push repsy/repsy-landing:"$1"
 
