268618f542f6ead1574af013befc11c5a40b2afc
abdullahbeker <abdullahbeker.54@hotmail.com>
2021-10-14 23:21:23 +0300

    fix workflow env

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-docker login -u "$DOCKER_HUB_LOGIN" -p "$DOCKER_HUB_TOKEN" docker.io
+docker login -u "$DOCKER_HUB_LOGIN" -p "$DOCKER_HUB_TOKEN"
 docker build . --file Dockerfile --tag repsy/repsy-landing:"$1"
 docker push repsy/repsy-landing:"$1"
 
